4e63244bbf8a9a50ff90e3d146a9d9ce4b697710
tmahan95 <tmahan95@users.noreply.github.com>
2017-11-20 15:18:02 -0500

    199 program 2

---
+++ Program2/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Program2
+{
+    partial class Prog2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTxtBox = new System.Windows.Forms.TextBox();
+            this.creditsTxtBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.regisButton = new System.Windows.Forms.Button();
+            this.regLbl = new System.Windows.Forms.Label();
+            this.regTAndDLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameTxtBox
+            // 
+            this.nameTxtBox.Location = new System.Drawing.Point(41, 77);
+            this.nameTxtBox.Name = "nameTxtBox";
+            this.nameTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTxtBox.TabIndex = 0;
+            // 
+            // creditsTxtBox
+            // 
+            this.creditsTxtBox.Location = new System.Drawing.Point(197, 77);
+            this.creditsTxtBox.Name = "creditsTxtBox";
+            this.creditsTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.creditsTxtBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Last Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter Total Credits Earned:";
+            // 
+            // regisButton
+            // 
+            this.regisButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.regisButton.Location = new System.Drawing.Point(131, 212);
+            this.regisButton.Name = "regisButton";
+            this.regisButton.Size = new System.Drawing.Size(75, 37);
+            this.regisButton.TabIndex = 4;
+            this.regisButton.Text = "Show Registration";
+            this.regisButton.UseVisualStyleBackColor = true;
+            this.regisButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // regLbl
+            // 
+            this.regLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.regLbl.Location = new System.Drawing.Point(107, 163);
+            this.regLbl.Name = "regLbl";
+            this.regLbl.Size = new System.Drawing.Size(123, 26);
+            this.regLbl.TabIndex = 5;
+            this.regLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // regTAndDLbl
+            // 
+            this.regTAndDLbl.AutoSize = true;
+            this.regTAndDLbl.Location = new System.Drawing.Point(104, 137);
+            this.regTAndDLbl.Name = "regTAndDLbl";
+            this.regTAndDLbl.Size = new System.Drawing.Size(136, 13);
+            this.regTAndDLbl.TabIndex = 6;
+            this.regTAndDLbl.Text = "Registration Time and Date";
+            // 
+            // Prog2
+            // 
+            this.AcceptButton = this.regisButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 261);
+            this.Controls.Add(this.regTAndDLbl);
+            this.Controls.Add(this.regLbl);
+            this.Controls.Add(this.regisButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.creditsTxtBox);
+            this.Controls.Add(this.nameTxtBox);
+            this.Name = "Prog2";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameTxtBox;
+        private System.Windows.Forms.TextBox creditsTxtBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button regisButton;
+        private System.Windows.Forms.Label regLbl;
+        private System.Windows.Forms.Label regTAndDLbl;
+    }
+}
+
---
+++ Program2/Form1.cs
@@ -0,0 +1,161 @@
+﻿//Timothy Mahan
+//Program 2
+//Due 3/8/16
+//Section 01
+/*This program takes the character entered in the first position of the text box and trys to parse it into a char variable.
+ *Next the credit hours entered by the user are entered and parsed into a float variable. Both of these pieces of information are then 
+ *tested to determine when a student should register based on the input they provide.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Prog2 : Form
+    {
+        public Prog2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //CONSTANTS
+            const byte SOPHOMORE = 30;//minimum hours needed to be a sophomore
+            const byte JUNIOR = 60;//minimum hours needed to be a junior
+            const byte SENIOR = 90;//minimum hours needed to be a senior
+
+            //User Input
+            float credits = 0;//store number of credit hours entered by student
+            char name; //stores first letter of last name
+
+            //Output Strings 
+
+            //These store time and day depending on credit hours and last name
+            string time = "default";
+            string day = "default";
+
+            //these times are the five possible periods for opening registration
+            string regtime1 = "8:30 AM";
+            string regtime2 = "10:00 AM";
+            string regtime3 = "11:30 AM";
+            string regtime4 = "2:00 PM";
+            string regtime5 = "4:00 PM";
+
+            //These are all of the possible days for opening registration
+            string regDay1 = "March 30";//senior registration day
+            string regDay2 = "March 31";//junior registration day
+            string regDay3 = "April 1";//first registration day for sophomores
+            string regDay4 = "April 4";//second registration day for sophomores
+            string regDay5 = "April 5";//first registration day for freshmen
+            string regDay6 = "April 6";//second registration day for freshmen
+
+            if (char.IsLetter(nameTxtBox.Text, 0))//test to see if the first digit in the nameTxtBox Textproperty is valid
+            {
+                name = char.ToUpper(nameTxtBox.Text[0]);
+                if (float.TryParse(creditsTxtBox.Text, out credits))//test to see if the credits Text property can be parsed as double.
+                {
+                    if (credits >= 0)
+                    {
+                        if (credits < JUNIOR)//test to see if a student is not a junior or senior
+                        {
+                            if (name >= 'E' && name <= 'F' || name >= 'R' && name <= 'S')//see what time a student may register based on name
+                            {
+                                time = regtime1;
+                            }
+                            else if (name >= 'G' && name <= 'I' || name >= 'T' && name <= 'V')
+                            {
+                                time = regtime2;
+                            }
+                            else if (name >= 'J' && name <= 'L' || name >= 'W' && name <= 'Z')
+                            {
+                                time = regtime3;
+                            }
+                            else if (name >= 'M' && name <= 'O' || name >= 'A' && name <= 'B')
+                            {
+                                time = regtime4;
+                            }
+                            else
+                            {
+                                time = regtime5;
+                            }
+                            if (credits < SOPHOMORE)//tests to see if freshmen
+                            {
+                                if (name >= 'E' && name <= 'Q')//sets day for freshmen depending on name's value
+                                {
+                                    day = regDay5;
+                                }
+                                else
+                                {
+                                    day = regDay6;
+                                }
+                            }
+                            else
+                            {
+                                if (name >= 'E' && name <= 'Q')//sets day for sophomore depending on name's value
+                                {
+                                    day = regDay3;
+                                }
+                                else
+                                {
+                                    day = regDay4;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (name >= 'E' || name <= 'I')//time student may register based on name value
+                            {
+                                time = regtime1;
+                            }
+                            else if (name >= 'J' || name <= 'O')
+                            {
+                                time = regtime2;
+                            }
+                            else if (name >= 'P' || name <= 'S')
+                            {
+                                time = regtime3;
+                            }
+                            else if (name >= 'T' || name <= 'Z')
+                            {
+                                time = regtime4;
+                            }
+                            else
+                            {
+                                time = regtime5;
+                            }
+                            if (credits < SENIOR)//determines whether junior or senior, sets day according to credits value.
+                            {
+                                day = regDay2;
+                            }
+                            else
+                            {
+                                day = regDay1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Total Credits Earned hours must be greater than or equal to 0.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please only enter non-negative numbers in the Credit Hours box.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please only enter text characters in the Last Name box.");
+            }
+            regLbl.Text = day + ", " + time;
+        }
+    }
+}
